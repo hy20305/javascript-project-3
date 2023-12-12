003ce5054dad6469aa3034b17bab8b0ce68f66d1
hy20305 <133744186+hy20305@users.noreply.github.com>
2023-12-12 22:47:53 +0900

    Add files via upload

---
+++ 16-2 기본통신코드-서버.js
@@ -0,0 +1,20 @@
+// 서버를 생성합니다.
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+// 서버를 실행합니다.
+server.listen(52273);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.emit('news', {
+        hello: 'world'
+    });
+    socket.on('other event', (data) => {
+        console.log(data);
+    });
+});
